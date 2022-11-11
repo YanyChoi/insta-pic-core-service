996d9e82cf26058d46f2512d6effc48b05eac17e
Yany Choi <51287461+YanyChoi@users.noreply.github.com>
2022-11-11 23:40:37 +0900

    added profile image uploading to s3

--- src/main/java/com/instantpic/coreservice/controller/UserController.java
+++ src/main/java/com/instantpic/coreservice/controller/UserController.java
@@ -1,10 +1,18 @@
 package com.instantpic.coreservice.controller;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.instantpic.coreservice.dto.user.UserDto;
 import com.instantpic.coreservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -12,11 +20,13 @@ import java.util.Optional;
 public class UserController {
 
     private UserService userService;
-
+    private AmazonS3Client amazonS3Client;
+    private String S3Bucket = "instapic-media";
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AmazonS3Client amazonS3Client) {
         this.userService = userService;
+        this.amazonS3Client = amazonS3Client;
     }
 
     @GetMapping("/login")
@@ -32,7 +42,23 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public UserDto signUp(@RequestBody UserDto userDraft) {
+    public UserDto signUp(@RequestPart UserDto userDraft, @RequestPart MultipartFile profilePic) throws IOException {
+
+        String originalName = profilePic.getOriginalFilename(); // 파일 이름
+        long size = profilePic.getSize(); // 파일 크기
+
+        ObjectMetadata objectMetaData = new ObjectMetadata();
+        objectMetaData.setContentType(profilePic.getContentType());
+        objectMetaData.setContentLength(size);
+
+        // S3에 업로드
+        amazonS3Client.putObject(
+                new PutObjectRequest(S3Bucket, originalName, profilePic.getInputStream(), objectMetaData)
+                        .withCannedAcl(CannedAccessControlList.PublicRead)
+        );
+
+        String imagePath = amazonS3Client.getUrl(S3Bucket, originalName).toString(); // 접근가능한 URL 가져오기
+        userDraft.setProfilePic(imagePath);
 
         UserDto user = userService.signupService(userDraft);
         return user;
