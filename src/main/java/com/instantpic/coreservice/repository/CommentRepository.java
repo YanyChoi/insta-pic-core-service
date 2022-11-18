f138936604fd9494b0a815adfd66a914f378c40f
Yany Choi <51287461+YanyChoi@users.noreply.github.com>
2022-11-18 18:44:21 +0900

    comment relations

--- src/main/java/com/instantpic/coreservice/repository/CommentRepository.java
+++ src/main/java/com/instantpic/coreservice/repository/CommentRepository.java
@@ -41,7 +41,7 @@ public class CommentRepository {
     public List<CommentDto> deleteCommentByCommentId(int commentId) {
         List<CommentDto> result = jdbcTemplate.query("SELECT comment.*, user.profile_pic, mention.user_id AS mentioned_id FROM instapic.comment AS comment INNER JOIN instapic.user AS user ON comment.user_id = user.user_id LEFT JOIN instapic.comment_mention AS mention ON comment.comment_id = mention.comment_id WHERE comment.comment_id = ?;", commentDtoRowMapper(), commentId);
         jdbcTemplate.update("DELETE FROM instapic.comment_mention WHERE comment_id = ?;", commentId);
-        jdbcTemplate.update("DELETE FROM instapic.comment WHERE comment_id = ?", commentId);
+        jdbcTemplate.update("DELETE FROM instapic.comment WHERE comment_id = ? OR parent_comment_id = ?;", commentId, commentId);
         return result;
     }
 
@@ -49,7 +49,7 @@ public class CommentRepository {
     public List<CommentDto> deleteCommentByArticleId(int articleId) {
         List<CommentDto> result = jdbcTemplate.query("SELECT comment.*, user.profile_pic, mention.user_id AS mentioned_id FROM instapic.comment AS comment INNER JOIN instapic.user AS user ON comment.user_id = user.user_id LEFT JOIN instapic.comment_mention AS mention ON comment.comment_id = mention.comment_id WHERE comment.article_id = ?", commentDtoRowMapper(), articleId);
         jdbcTemplate.update("DELETE FROM instapic.comment_mention WHERE comment_id IN (SELECT comment_id FROM instapic.comment WHERE article_id = ?);", articleId);
-        jdbcTemplate.update("DELETE FROM instapic.comment WHERE article_id = ?", articleId);
+        jdbcTemplate.update("DELETE FROM instapic.comment WHERE article_id = ?;", articleId);
         return result;
     }
 
